de5c881fb356c55e9fc11bd4279d5c8e402ce53b
CyrilGodet <cyril.godet.etu@univ-lille.fr>
2026-02-27 20:20:47 +0100

    feat:ajout token jwt

--- utilisateurs/src/auth/auth.module.ts
+++ utilisateurs/src/auth/auth.module.ts
@@ -1,17 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
 import { Auth } from './entities/auth.entity';
 import { Role } from './entities/role.entity';
+import { jwtConstants } from './constants';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Auth, Role])
+    TypeOrmModule.forFeature([User, Auth, Role]),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
-  exports: [AuthService]
+  exports: [AuthService],
 })
 export class AuthModule {}
